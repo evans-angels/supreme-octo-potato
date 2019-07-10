5207f53048d3f777edf7544ffdfad966a67956a4
John Sigmon <johnasigmon@gmail.com>
2019-07-10 17:49:55 -0400

    wip chaos

--- supreme-octo-potato/src/App.js
+++ supreme-octo-potato/src/App.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import Input from './components/input';
-import ImageContainer from './components/imagesContainer';
+import React, { Component } from "react";
+import Input from "./components/input";
+import ImageContainer from "./components/imagesContainer";
 import Header from "./components/Header.js";
 import { calculateVariantCount, getAllVariations } from "./utils/helpers.js";
-// import OutputContainer from "./containers/OutputContainer.js";
-// import OutputControl from "./containers/OutputControl.js";
-// import QueuedParamList from "./components/QueuedParamList.js";
+import OutputContainer from "./containers/OutputContainer.js";
+import OutputControl from "./containers/OutputControl.js";
+import QueuedParamList from "./components/QueuedParamList.js";
 import ImageSource from "./components/ImageSource.js";
-// import Geolocation from "./components/GeoLocation.js";
-// import ParamPicker from "./components/ParamPicker.js";
-// import ParamInput from "./components/ParamInput.js";
-// import logo from './logo.svg';
-import './App.css';
-import $ from 'jquery';
+import Geolocation from "./components/GeoLocation.js";
+import ParamPicker from "./components/ParamPicker.js";
+import ParamInput from "./components/ParamInput.js";
+import logo from './logo.svg';
+import "./App.css";
+import $ from "jquery";
 
 class App extends Component {
   constructor(props) {
@@ -26,6 +26,37 @@ class App extends Component {
         validatedSourceUrl: "",
         csvUploadedOrSkipped: false
       },
+      options: {
+        hasGeolocation: false,
+        hasParams: false,
+        usingProxies: false,
+      },
+      actions: {
+        showInBrowser: false,
+        showPDF: false,
+        showUrls: false
+      },
+      // params: {
+      //   currentParam: {
+      //     text: "",
+      //     key: ""
+      //   },
+      //   rawSingleParams: [],
+      //   geolocationParams: [],
+      //   fullQueryCombinations: [],
+      //   finalizedUrls: [],
+      // },
+
+      activeParams: [],
+      params: [],
+      fullQueryStrings: [],
+      fullUrls: [],
+      proxyUrls: [],
+      currentParam: {
+        text: "",
+        key: ""
+      },
+      currentLocationParams: [],
     };
     this.grabImages = this.grabImages.bind(this);
     this.setSelectedImage = this.setSelectedImage.bind(this);
@@ -40,7 +71,6 @@ class App extends Component {
       .then(res => res.text())
       .then(data => {
         try {
-
           // data = data.replace(/<script src=/g, '<noscript class="mi-script-src" data-script-url=')
           // .replace(/<script/g, '<noscript class="mi-script-inline"')
           // .replace(/<\/script/g, "</noscript")
@@ -49,31 +79,79 @@ class App extends Component {
           // .replace(/srcset=/g, "data-srcset=");
 
           const $doc = $(data);
-          const imageTags = $doc.find('img');
+          const imageTags = $doc.find("img");
           if (!imageTags.length) {
-            throw new Error('no images found');
+            throw new Error("no images found");
           }
 
           const miImageTags = imageTags.filter((i, v) => {
-            const src = $(v).attr('src');
-            return src.indexOf('.com/p/rp') > -1;
+            const src = $(v).attr("src");
+            return src.indexOf(".com/p/rp") > -1;
           });
           if (!miImageTags.length) {
-            alert('no mi images found');
-            throw new Error('no mi images found');
+            alert("no mi images found");
+            throw new Error("no mi images found");
           }
 
           const images = $.map(miImageTags, (v, i) => {
-            return $(v).attr('src');
+            return $(v).attr("src");
           });
 
-          this.setState({images});
-
-        } catch(e) {
+          this.setState({ images });
+        } catch (e) {
           console.log(e);
         }
-
       });
+  }
+
+  viewInBroswer = e => {
+    this.setState({ actions: { showInBrowser: !this.state.actions.showInBrowser } });
+  };
+
+  generatePDF = e => {
+    this.setState({ actions: { showPDF: !this.state.showPDF } });
+  };
+
+  showUrls = e => {
+    this.setState({ actions: { showUrls: !this.state.showUrls } });
+  };
+
+  toggleProxies = e => {
+    this.setState({ options: { usingProxies: !this.state.usingProxies } });
+  };
+
+  handleInputSourceChange = e => {
+    const imgSource = e.target.value;
+    const csvUploadedOrSkipped = this.state.rulesToRun.csvUploadedOrSkipped;
+    // validate to make sure its an image
+    if ((imgSource + "").indexOf(".png") > -1) {
+      // const newState = Object.m
+      this.setState({
+        rulesToRun: {
+          hasValidatedSourceURL: true,
+          validatedSourceUrl: imgSource,
+          csvUploadedOrSkipped
+        }
+      });
+      return;
+    }
+    // this.setState({ rulesToRun: { validatedSourceUrl: imgSource } });
+  };
+
+  resetState(editParam) {
+    const currentParam = editParam.length ? editParam[0] : { text: "", key: "" }; //BUGGGG
+    this.setState({
+      params: [],
+      totalContentCount: 0,
+      fullQueryStrings: [],
+      fullUrls: [],
+      currentParam: currentParam,
+      proxyUrls: []
+    });
+  }
+
+  getProxyUrls(fullUrls) {
+    //return fullUrls.map(async url => await CD.proxyUrl(url));
   }
 
   updateStateParams(params, editParam, geolocationParams) {
@@ -112,46 +190,197 @@ class App extends Component {
     }
   }
 
-  handleInputSourceChange = e => {
-    const imgSource = e.target.value;
-    const csvUploadedOrSkipped = this.state.rulesToRun.csvUploadedOrSkipped;
-    // validate to make sure its an image
-    if ((imgSource + "").indexOf(".png") > -1) {
-      // const newState = Object.m
-      this.setState({
-        rulesToRun: {
-          hasValidatedSourceURL: true,
-          validatedSourceUrl: imgSource,
-          csvUploadedOrSkipped
-        }
-      });
-      return;
+  addParam = e => {
+    e.preventDefault();
+    const addParam = this.state.currentParam;
+    const geolocationParams = this.state.currentLocationParams;
+    if (addParam.text !== "") {
+      const params = [...this.state.params, addParam];
+      return this.updateStateParams(params, [], geolocationParams);
     }
-    // this.setState({ rulesToRun: { validatedSourceUrl: imgSource } });
+  };
+
+  editParam = key => {
+    const editParam = this.state.params.filter(param => param.key === key);
+    const filteredParams = this.state.params.filter(param => param.key !== key);
+    const geolocationParams = this.state.currentLocationParams;
+    if (filteredParams.length) {
+      return this.updateStateParams(filteredParams, editParam, geolocationParams);
+    } else {
+      return this.resetState([], editParam, geolocationParams);
+    }
+  };
+
+  deleteParam = key => {
+    const filteredParams = this.state.params.filter(param => param.key !== key);
+    const geolocationParams = this.state.currentLocationParams;
+    if (filteredParams.length) {
+      return this.updateStateParams(filteredParams, [], geolocationParams);
+    } else {
+      return this.resetState(filteredParams, [], geolocationParams);
+    }
+  };
+
+  handleInput = e => {
+    const paramName = e.target.value;
+    const currentParam = { text: paramName, key: Date.now() };
+    this.setState({
+      currentParam
+    });
+  };
+
+  skipCsvUpload = e => {
+    const csvUploadedOrSkipped = !this.state.rulesToRun.csvUploadedOrSkipped;
+    const { hasValidatedSourceURL, validatedSourceUrl } = this.state.rulesToRun;
+
+    this.setState({
+      rulesToRun: {
+        hasValidatedSourceURL: hasValidatedSourceURL,
+        validatedSourceUrl: validatedSourceUrl,
+        csvUploadedOrSkipped: csvUploadedOrSkipped
+      }
+    });
+  };
+
+
+
+  handleLocationParams = e => {
+    // console.log("city selected");
+    // console.log(e);
+    const inputLocation = e;
+    // console.log(this.state)
+    const duplicateCheck = this.state.currentLocationParams.filter(location => location === inputLocation);
+    const geolocationParams = duplicateCheck.length
+      ? this.state.currentLocationParams.filter(location => location !== inputLocation)
+      : [...this.state.currentLocationParams, inputLocation];
+    const params = this.state.params || [];
+    return this.updateStateParams(params, [], geolocationParams);
+  };
+
+  handleCSVUpload = data => {
+    const mergeParamArray = data[0];
+    const paramValues = data.slice(1, data.length);
+
+    const paramBreakOut = mergeParamArray.map((param, idx) => {
+      let allValues = [];
+      if (param) {
+        paramValues.forEach(value => {
+          if (value && value[idx]) {
+            let paramValue = value[idx];
+            if (value[idx] === "TRUE") {
+              paramValue = "true";
+            } else if (value[idx] === "FALSE") {
+              paramValue = "false";
+            }
+            allValues.push(paramValue);
+          }
+        });
+      }
+
+      return `${param}=${allValues.join("|")}`;
+    });
+    // console.log('look here')
+    // console.log(paramBreakOut)
+    paramBreakOut.forEach(uploadParam => {
+      const currentParam = { text: uploadParam, key: Date.now() };
+      // console.log("indvidual param");
+      // console.log(currentParam);
+      this.setState({
+        currentParam
+      });
+      const geolocationParams = this.state.geolocationParams;
+      const addParam = this.state.currentParam;
+      const params = [...this.state.params, addParam];
+      // console.log(params);
+      return this.updateStateParams(params, [], geolocationParams);
+    });
+
+    const csvUploadedOrSkipped = !this.state.rulesToRun.csvUploadedOrSkipped;
+    const validatedSourceUrl = this.state.rulesToRun.validatedSourceUrl;
+    const hasValidatedSourceURL = this.state.rulesToRun.hasValidatedSourceURL;
+    this.setState({
+      rulesToRun: {
+        hasValidatedSourceURL: hasValidatedSourceURL,
+        validatedSourceUrl: validatedSourceUrl,
+        csvUploadedOrSkipped: csvUploadedOrSkipped
+      }
+    });
   };
 
   render() {
     const count = this.state.totalContentCount;
     const tooManyVariants = count > 100;
     const selectedImage = this.state.selectedImage;
+    const params = this.state.params;
     const {
       rulesToRun: { hasValidatedSourceURL, validatedSourceUrl, csvUploadedOrSkipped }
     } = this.state;
+    const renderOutputControls = hasValidatedSourceURL && !!params.length;
+    const renderParamListAndControls = hasValidatedSourceURL && csvUploadedOrSkipped;
+    const renderParamPicker = !renderParamListAndControls && hasValidatedSourceURL;
     return (
       <div className="App">
+        <header className="App-header">
         <Header count={count} tooManyVariants={tooManyVariants} />
         <div className="container">
-        {!selectedImage ? (
-          <>
-          <Input grabImages={this.grabImages}></Input>
-          <ImageContainer setSelectedImage={this.setSelectedImage} images={this.state.images}/>
-          )
-              </>
-            ) :
+          {!selectedImage ? (
+            <>
+              <Input grabImages={this.grabImages} />
+              <ImageContainer setSelectedImage={this.setSelectedImage} images={this.state.images} />)
+            </>
+          ) : (
             <ImageSource validatedSourceUrl={selectedImage} onInputSourceChange={this.handleInputSourceChange} />
-            }
+          )}
 
+{renderParamPicker && (
+              <>
+                <QueuedParamList 
+                  params={params} 
+                  deleteParam={this.deleteParam} 
+                  editParam={this.editParam} 
+                  handleLocationParams={this.handleLocationParams}
+                  currentLocationParams={this.currentLocationParams}
+                  />
+                <ParamPicker
+                  csvUploadedOrSkipped={csvUploadedOrSkipped}
+                  handleCSVUpload={this.handleCSVUpload}
+                  skipCsvUpload={this.skipCsvUpload}
+                />
+              </>
+            )}
+
+            {renderParamListAndControls && (
+              <>
+                <QueuedParamList params={params} deleteParam={this.deleteParam} editParam={this.editParam} />
+                <Geolocation handleLocationParams={this.handleLocationParams} currentLocationParams={this.currentLocationParams} />
+                <ParamInput
+                  addParam={this.addParam}
+                  inputElement={this.inputElement}
+                  handleInput={this.handleInput}
+                  currentParam={this.state.currentParam}
+                />
+              </>
+            )}
+          {renderOutputControls && (
+            <OutputControl
+              hasValidatedSourceURL={hasValidatedSourceURL}
+              tooManyVariants={tooManyVariants}
+              generatePDF={this.generatePDF}
+              viewInBroswer={this.viewInBroswer}
+              showUrls={this.showUrls}
+            />
+          )}
+          <OutputContainer
+            validatedSourceUrl={validatedSourceUrl}
+            showPDF={this.state.actions.showPDF}
+            showInBrowser={this.state.actions.showInBrowser}
+            proxyUrls={this.state.proxyUrls}
+            fullQueryStrings={this.state.fullQueryStrings}
+            showUrls={this.state.actions.showUrls}
+            fullUrls={this.state.fullUrls}
+          />
         </div>
+          </header>
       </div>
     );
   }
