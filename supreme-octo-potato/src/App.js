133c7e66e0bc65ff68e0ffca5c886b4e47f60073
John Sigmon <johnasigmon@gmail.com>
2019-07-10 17:36:03 -0400

    jeffs component is feeding mine

--- supreme-octo-potato/src/App.js
+++ supreme-octo-potato/src/App.js
@@ -3,6 +3,13 @@ import Input from './components/input';
 import ImageContainer from './components/imagesContainer';
 import Header from "./components/Header.js";
 import { calculateVariantCount, getAllVariations } from "./utils/helpers.js";
+// import OutputContainer from "./containers/OutputContainer.js";
+// import OutputControl from "./containers/OutputControl.js";
+// import QueuedParamList from "./components/QueuedParamList.js";
+import ImageSource from "./components/ImageSource.js";
+// import Geolocation from "./components/GeoLocation.js";
+// import ParamPicker from "./components/ParamPicker.js";
+// import ParamInput from "./components/ParamInput.js";
 // import logo from './logo.svg';
 import './App.css';
 import $ from 'jquery';
@@ -12,7 +19,13 @@ class App extends Component {
     super(props);
     this.state = {
       images: null,
-      selectedImage: null
+      selectedImage: null,
+      totalContentCount: 0,
+      rulesToRun: {
+        hasValidatedSourceURL: false, ///need to wire up
+        validatedSourceUrl: "",
+        csvUploadedOrSkipped: false
+      },
     };
     this.grabImages = this.grabImages.bind(this);
     this.setSelectedImage = this.setSelectedImage.bind(this);
@@ -63,16 +76,81 @@ class App extends Component {
       });
   }
 
+  updateStateParams(params, editParam, geolocationParams) {
+    // console.log("STATE UPDATE");
+    // console.log(params);
+    const currentParam = editParam.length ? editParam[0] : { text: "", key: "" };
+    const fullQueryStrings = getAllVariations(params, geolocationParams);
+    const fullUrls = fullQueryStrings.map(q => `${this.state.rulesToRun.validatedSourceUrl}${q}`);
+    const currentLocationParams = this.state.currentLocationParams;
+    if (this.state.options.usingProxies) {
+      const promiseProxies = this.getProxyUrls(fullUrls);
+
+      Promise.all(promiseProxies).then(proxyUrls => {
+        // console.log('inside the promise')
+        // console.log(params)
+        this.setState({
+          params: params,
+          totalContentCount: calculateVariantCount(params),
+          fullQueryStrings: fullQueryStrings,
+          fullUrls: fullUrls,
+          currentParam: currentParam,
+          proxyUrls: proxyUrls,
+          currentLocationParams: currentLocationParams
+        });
+      });
+    } else {
+      this.setState({
+        params: params,
+        totalContentCount: calculateVariantCount(params),
+        fullQueryStrings: fullQueryStrings,
+        fullUrls: fullUrls,
+        currentParam: currentParam,
+        proxyUrls: fullUrls,
+        currentLocationParams: currentLocationParams
+      });
+    }
+  }
+
+  handleInputSourceChange = e => {
+    const imgSource = e.target.value;
+    const csvUploadedOrSkipped = this.state.rulesToRun.csvUploadedOrSkipped;
+    // validate to make sure its an image
+    if ((imgSource + "").indexOf(".png") > -1) {
+      // const newState = Object.m
+      this.setState({
+        rulesToRun: {
+          hasValidatedSourceURL: true,
+          validatedSourceUrl: imgSource,
+          csvUploadedOrSkipped
+        }
+      });
+      return;
+    }
+    // this.setState({ rulesToRun: { validatedSourceUrl: imgSource } });
+  };
+
   render() {
+    const count = this.state.totalContentCount;
+    const tooManyVariants = count > 100;
+    const selectedImage = this.state.selectedImage;
+    const {
+      rulesToRun: { hasValidatedSourceURL, validatedSourceUrl, csvUploadedOrSkipped }
+    } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src='https://d2j7qxqaxrrv1i.cloudfront.net/assets/logos/m-brandmark@2x-ff5e88fbcd5884186e67f4acc5d7d9645571be5902e2f9d7825f4ee8080b7bf9.png'
-           className="App-logo" alt="logo" />
-        </header>
+        <Header count={count} tooManyVariants={tooManyVariants} />
         <div className="container">
+        {!selectedImage ? (
+          <>
           <Input grabImages={this.grabImages}></Input>
           <ImageContainer setSelectedImage={this.setSelectedImage} images={this.state.images}/>
+          )
+              </>
+            ) :
+            <ImageSource validatedSourceUrl={selectedImage} onInputSourceChange={this.handleInputSourceChange} />
+            }
+
         </div>
       </div>
     );
